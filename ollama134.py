07ac754ff1c7758400c69d79dc211f58c6891c87
suman <suman.kumar@thetechclouds.com>
2025-05-23 18:49:30 +0530

    streamlit working but got error in langsmith

--- ollama134.py
+++ ollama134.py
@@ -2,6 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms.ollama import Ollama
+from langchain_ollama import OllamaLLM
 
 import streamlit as st
 import os
@@ -27,7 +28,8 @@ st.title('Langchain Demo With llama2 API')
 input_text=st.text_input("Search the topic u want")
 
 # openAI LLm 
-llm=Ollama(model="gemma3:1b")
+#llm=Ollama(model="gemma3:1b")
+llm = OllamaLLM(model="gemma3:1b")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
